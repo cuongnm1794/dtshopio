20faf934b93fbdfa0caad8c3afe4f67a9dd42e06
CuongDev <cuongnm1794@gmail.com>
2024-04-02 08:34:33 +0700

    add to list

--- tau_nhanh.php
+++ tau_nhanh.php
@@ -29,39 +29,42 @@ include "./top.php";
     </div>
 </div>
 <script>
-// add event button go
-document.getElementById("go").addEventListener("click", function() {
+    // add event button go
+    document.getElementById("go").addEventListener("click", function() {
 
-    console.log("go");
+        console.log("go");
 
-    // get links
-    var links = document.getElementById("links").value;
-    // split new line to array
-    links = links.split("\n");
+        // get links
+        var links = document.getElementById("links").value;
+        // split new line to array
+        links = links.split("\n");
 
 
-    // loop links
-    links.forEach(function(link, key) {
+        // loop links
+        links.forEach(function(link, key) {
 
-        let link_sample = "https://online.nojima.co.jp/app/catalog/detail/addcart/1/" + link +
-            "?quantity=1&shopCode=1&giftCode=99&optionCommodity=99&selectSkuCode=" + link +
-            "&reorderFlg=true&shippingShopCode=1&oldAddreessNo=0&shippingAddress=928782&deliveryTypeCode=0"
+            let link_sample = "https://online.nojima.co.jp/app/catalog/detail/addcart/1/" + link +
+                "?quantity=1&shopCode=1&giftCode=99&optionCommodity=99&selectSkuCode=" + link +
+                "&reorderFlg=true&shippingShopCode=1&oldAddreessNo=0&shippingAddress=928782&deliveryTypeCode=0"
 
-        // create li
-        var li = document.createElement("li");
-        // create a
-        var a = document.createElement("a");
-        // set href
-        a.href = link_sample;
-        // set text
-        a.innerText = link_sample;
-        // append to li
-        li.appendChild(a);
-        // append to ul
-        document.getElementById("list-links").appendChild(li);
+            // create li
+            var li = document.createElement("li");
+            // create a
+            var a = document.createElement("a");
+            // set href
+            a.href = link_sample;
+            // set text
+            a.innerText = link_sample;
 
+            // taget blank
+            a.target = "_blank";
+            // append to li
+            li.appendChild(a);
+            // append to ul
+            document.getElementById("list-links").appendChild(li);
+
+        });
     });
-});
 </script>
 
 <?php
